e0c5ce051e129f63f428e25c0322d2f702ee8f0b
johnngure <johnwngure@gmail.com>
2023-12-09 13:57:12 +0300

    CurrencyConverter

--- Currency.py
+++ Currency.py
@@ -1,10 +1,15 @@
 import requests
 
-API_KEY = ''
-BASE_URL = f"={API_KEY}"
+API_KEY = 'fca_live_xijkcU8fjFFaJhIR0Ol5nGc3uvl2k1769RaORtjQ'
+BASE_URL = f"https://app.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
 
-CURRENCIES = ["USD", "EUR", "KSH", "TSH", "USH", "RAND"]
+CURRENCIES = ["USD", "EUR", "KSH", "TSH", "USH", "RAND", "CNY", "KW"]
 
 def convert_currency(base):
     currencies = ",".join(CURRENCIES)
-    
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+    try:
+        
+        
+    except:
+        pass       
