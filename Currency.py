e88069f6a439ec4221b5649f613837b7664f0553
johnngure <johnwngure@gmail.com>
2023-12-11 22:14:57 +0300

    CurrencyConverter

--- Currency.py
+++ Currency.py
@@ -9,7 +9,10 @@ def convert_currency(base):
     currencies = ",".join(CURRENCIES)
     url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
     try:
-        
-        
-    except:
-        pass       
+       response = requests.get(url)
+       data = response.json(data)
+       print(data)
+       return data
+    except Exception as e:
+        print(e)
+        return None
